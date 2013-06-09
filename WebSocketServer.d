24cc83963ac9bdb241e4ec65983e0aeb83090f4c
ey.engin.1991@googlemail.com <ey.engin.1991@googlemail.com@a71ced74-7187-ceb6-e153-6737c5748972>
2013-06-09 14:56:15 +0000

    commit work from 8.6.13 in the university

--- WebSocketServer.d
+++ WebSocketServer.d
@@ -1,3 +1,7 @@
 WebSocketServer.o: WebSocketServer.cpp WebSocketServer.h \
  ../../core/AmThread.h ../../core/singleton.h ../../core/AmThread.h \
- ../../core/log.h
+ ../../core/log.h ../../core/sip/sip_parser.h ../../core/sip/cstring.h \
+ ../../core/sip/parse_uri.h ../../core/sip/resolver.h \
+ ../../core/hash_table.h ../../core/log.h ../../core/atomic_types.h \
+ ../../core/sip/parse_dns.h ../../core/sip/parse_next_hop.h \
+ ../../core/sip/trans_layer.h
