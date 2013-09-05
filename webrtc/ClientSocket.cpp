b7050deb440695977540095bc71d51193bf0a8ad
ey.engin.1991@googlemail.com <ey.engin.1991@googlemail.com@a71ced74-7187-ceb6-e153-6737c5748972>
2013-09-05 20:41:53 +0000

    add new branch

---
+++ webrtc/ClientSocket.cpp
@@ -0,0 +1,27 @@
+#include "ClientSocket.h"
+
+ClientSocket::ClientSocket(ServerSocket* server_sock,int sd, sockaddr_storage* sa)
+: trsp_socket(server_sock->get_if(),0,0,sd)
+{
+	mServer_sock = server_sock;
+}
+
+ClientSocket::~ClientSocket()
+{
+
+}
+
+int ClientSocket::send(const sockaddr_storage* sa, const char* msg, const int msg_len)
+{
+	return 0;
+}
+
+int ClientSocket::bind(const string& address, unsigned short port)
+{
+	return 0;
+}
+
+const char* ClientSocket::get_transport() const
+{
+	return mServer_sock->get_transport();
+}
---
+++ webrtc/ServerSocket.d
@@ -0,0 +1,3 @@
+ServerSocket.o: ServerSocket.cpp ServerSocket.h \
+ ../../core/sip/transport.h ../../core/sip/../AmThread.h \
+ ../../core/sip/../atomic_types.h ../../core/sip/../log.h
