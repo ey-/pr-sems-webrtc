95215554cdd0fd74afc2ca55c5d270341ff3dd24
ey.engin.1991@googlemail.com <ey.engin.1991@googlemail.com@a71ced74-7187-ceb6-e153-6737c5748972>
2013-09-05 20:41:05 +0000

    add new branch

--- Makefile
+++
@@ -1,6 +0,0 @@
-plug_in_name = websocket
-
-module_ldflags = -lwebsockets
-
-COREPATH ?=../../core
-include $(COREPATH)/plug-in/Makefile.app_module
--- WebSocketServer.d
+++
@@ -1,9 +0,0 @@
-WebSocketServer.o: WebSocketServer.cpp WebSocketServer.h \
- ../../core/AmThread.h ../../core/singleton.h ../../core/AmThread.h \
- ../../core/sip/sip_parser.h ../../core/sip/cstring.h \
- ../../core/sip/parse_uri.h ../../core/sip/resolver.h \
- ../../core/hash_table.h ../../core/log.h ../../core/atomic_types.h \
- ../../core/sip/parse_dns.h ../../core/sip/parse_next_hop.h \
- ../../core/sip/transport.h ../../core/sip/../AmThread.h \
- ../../core/sip/../atomic_types.h ../../core/log.h \
- ../../core/sip/trans_layer.h
